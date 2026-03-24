118422044086cb34a3dcafa2a38a2e666a4a50bd
Mark Hingston <mark.hingston@puregym.com>
2026-03-24 19:36:56 +0000

    Replace flutter_markdown with flutter_markdown_plus (discontinued)

--- lib/src/widgets/chat_markdown.dart
+++ lib/src/widgets/chat_markdown.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:flutter_markdown_plus/flutter_markdown_plus.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 import '../theme/app_theme.dart';
--- test/chat_message_bubble_test.dart
+++ test/chat_message_bubble_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:flutter_markdown_plus/flutter_markdown_plus.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:openchat/src/models/attachment.dart';
 import 'package:openchat/src/models/chat_message.dart';
